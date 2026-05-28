c55abb1ea25c34dfd9d87876d829a7086a0bfabc
tanaysaraf <51254578+tanaysaraf@users.noreply.github.com>
2026-05-28 17:42:02 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    void solve(vector<vector<char>>& grid, int i, int j)
+    {
+        if(i<0 or j<0 or i==grid.size() or j==grid[0].size() or grid[i][j]=='0')
+        return;
+
+        //cout<<i<<" "<<j<<endl;
+        grid[i][j]='0';
+        solve(grid, i+1,j);
+        solve(grid, i-1,j);
+        solve(grid, i,j+1);
+        solve(grid, i,j-1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int ans=0;
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    //cout<<i<<" "<<j<<endl;
+                    //cout<<"----------------\n";
+                    solve(grid,i,j);
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
