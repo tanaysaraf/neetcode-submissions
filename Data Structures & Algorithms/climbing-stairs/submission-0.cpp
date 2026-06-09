c56f8631a35aec6697485613f7d5041d0f176f51
tanaysaraf <51254578+tanaysaraf@users.noreply.github.com>
2026-06-09 08:56:36 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,19 @@
+#include <string.h>
+class Solution {
+public:
+    int dp[50];
+    int solve(int n)
+    {
+        if(n==0) return 1;
+        if(n<0) return 0;
+
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n] = solve(n-1)+solve(n-2);
+    }
+    int climbStairs(int n) {
+
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
